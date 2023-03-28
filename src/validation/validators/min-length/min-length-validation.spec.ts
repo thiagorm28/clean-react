6f1be1ec50c0aa3394fcdfcf28bcacd8d5bb06f0
Thiago <thiago.moraes@gazin.com.br>
2023-03-28 18:23:02 -0300

    refactor: creating sut for tests

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -2,16 +2,18 @@ import { InvalidFieldError } from '@/validation/errors'
 import { faker } from '@faker-js/faker'
 import { MinLengthValidation } from './min-length-validation'
 
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
+
 describe('MinLenghtValition', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new MinLengthValidation(faker.random.word(), 5)
-    const error = sut.validate('123')
+    const sut = makeSut()
+    const error = sut.validate(faker.random.alphaNumeric(4))
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falsy if email is valid', () => {
-    const sut = new MinLengthValidation(faker.random.word(), 5)
-    const error = sut.validate('12345')
+    const sut = makeSut()
+    const error = sut.validate(faker.random.alphaNumeric(5))
     expect(error).toBeFalsy()
   })
 })
