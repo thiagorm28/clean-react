af7e792639e74d7ab30f896ba25102936fc79b90
Thiago <thiago.moraes@gazin.com.br>
2023-03-29 09:55:07 -0300

    fix: ensure email validation allow empty value

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -5,13 +5,13 @@ import { MinLengthValidation } from './min-length-validation'
 const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
 
 describe('MinLenghtValition', () => {
-  test('Should return error if email is invalid', () => {
+  test('Should return error if lenght is invalid', () => {
     const sut = makeSut()
     const error = sut.validate(faker.random.alphaNumeric(4))
     expect(error).toEqual(new InvalidFieldError())
   })
 
-  test('Should return falsy if email is valid', () => {
+  test('Should return falsy if lenght is valid', () => {
     const sut = makeSut()
     const error = sut.validate(faker.random.alphaNumeric(5))
     expect(error).toBeFalsy()
