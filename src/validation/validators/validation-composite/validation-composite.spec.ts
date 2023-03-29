92e8a4071cf2da972ed06959edf67ee935045983
Thiago <thiago.moraes@gazin.com.br>
2023-03-29 10:23:32 -0300

    test: ensure ValidationComposite returns the first error throwed

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -8,10 +8,11 @@ const makeSut = (validators: FieldValidation[]): ValidationComposite => new Vali
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error_message')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_error_message')
+    fieldValidationSpy2.error = new Error('second_error_message')
     const sut = makeSut([fieldValidationSpy, fieldValidationSpy2])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_error_message')
+    expect(error).toBe('first_error_message')
   })
 })
