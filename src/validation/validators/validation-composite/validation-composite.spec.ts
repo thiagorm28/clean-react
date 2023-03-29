40d80b64c303015362f544097ba6c01f5edb9ecb
Thiago <thiago.moraes@gazin.com.br>
2023-03-29 16:26:36 -0300

    test: ensure validation composite returns falsy if there is no error

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -8,8 +8,8 @@ type SutTypes = {
   fieldValidationsSpy: FieldValidationSpy[]
 }
 
-const makeSut = (): SutTypes => {
-  const fieldValidationsSpy = [new FieldValidationSpy('any_field'), new FieldValidationSpy('any_field')]
+const makeSut = (fieldName: string = faker.database.column()): SutTypes => {
+  const fieldValidationsSpy = [new FieldValidationSpy(fieldName), new FieldValidationSpy(fieldName)]
   const sut = new ValidationComposite(fieldValidationsSpy)
   return {
     sut,
@@ -19,10 +19,18 @@ const makeSut = (): SutTypes => {
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = makeSut()
-    fieldValidationsSpy[0].error = new Error('first_error_message')
-    fieldValidationsSpy[1].error = new Error('second_error_message')
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationsSpy } = makeSut(fieldName)
+    const errorMessage = faker.random.words()
+    fieldValidationsSpy[0].error = new Error(errorMessage)
+    fieldValidationsSpy[1].error = new Error(faker.random.words())
+    const error = sut.validate(fieldName, fieldName)
+    expect(error).toBe(errorMessage)
+  })
+
+  test('Should return falsy if there is no error', () => {
+    const { sut } = makeSut()
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('first_error_message')
+    expect(error).toBeFalsy()
   })
 })
