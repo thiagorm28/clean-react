bf4368f9e0ab539413fbed4fa24c77bca05989f6
Thiago <thiago.moraes@gazin.com.br>
2023-03-29 10:22:21 -0300

    test: ensure ValidationCmposite returns error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,17 @@
+import { FieldValidation } from '@/validation/protocols/field-validation'
+import { faker } from '@faker-js/faker'
+import { FieldValidationSpy } from '../test/mock-field-validation'
+import { ValidationComposite } from './validation-composite'
+
+const makeSut = (validators: FieldValidation[]): ValidationComposite => new ValidationComposite(validators)
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails', () => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    fieldValidationSpy2.error = new Error('any_error_message')
+    const sut = makeSut([fieldValidationSpy, fieldValidationSpy2])
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('any_error_message')
+  })
+})
