a9c74e6488c8f4fdb7178b247801f807c7cd3575
Thiago <thiago.moraes@gazin.com.br>
2023-03-29 18:28:58 -0300

    test: ensure ValidationBuilder has email and minLenght helper

--- src/validation/validators/build/validation-builder.spec.ts
+++ src/validation/validators/build/validation-builder.spec.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker'
-import { RequiredFieldValidation } from '@/validation/validators'
+import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validation/validators'
 import { ValidationBuilder } from './validation-builder'
 
 describe('ValidationBuilder', () => {
@@ -7,5 +7,18 @@ describe('ValidationBuilder', () => {
     const fieldName = faker.database.column()
     const validations = ValidationBuilder.field(fieldName).required().build()
     expect(validations).toEqual([new RequiredFieldValidation(fieldName)])
+  })
+
+  test('Should return EmailFieldValidation', () => {
+    const fieldName = faker.database.column()
+    const validations = ValidationBuilder.field(fieldName).email().build()
+    expect(validations).toEqual([new EmailValidation(fieldName)])
+  })
+
+  test('Should return MinLengthValidation', () => {
+    const fieldName = faker.database.column()
+    const minLenght = 5
+    const validations = ValidationBuilder.field(fieldName).minLenght(minLenght).build()
+    expect(validations).toEqual([new MinLengthValidation(fieldName, minLenght)])
   })
 })
