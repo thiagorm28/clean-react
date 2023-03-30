8cbc085d89eb9b1ee7e6efaee38989edd78f3eab
Thiago <thiago.moraes@gazin.com.br>
2023-03-30 19:25:42 -0300

    feat: creating factory for local storage

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,7 +2,14 @@ import { Login } from '@/presentation/pages'
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
 import React from 'react'
 import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
+import { makeSaveAccessToken } from '@/main/factories/usecases/save-access-token/save-access-token-factory'
 
 export const makeLogin: React.FC = () => {
-  return <Login authentication={makeRemoteAuthentication()} validation={makeLoginValidation()} />
+  return (
+    <Login
+      authentication={makeRemoteAuthentication()}
+      validation={makeLoginValidation()}
+      saveAccessToken={makeSaveAccessToken()}
+    />
+  )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -5,7 +5,7 @@ import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
 import { Authentication } from '@/domain/usecases'
 import { Link, useNavigate } from 'react-router-dom'
-import { SaveAccessToken } from '@/presentation/test'
+import { SaveAccessToken } from '@/domain/usecases/save-access-token'
 
 type StateProps = {
   isLoading: boolean
